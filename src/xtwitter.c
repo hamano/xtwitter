7fbbadadb5a2a3ab4fc95e694f4c9c3b9ec86e69
Tsukasa Hamano <code@cuspy.org>
2008-11-12 01:05:14 +0900

    release 0.3

--- src/xtwitter.c
+++ src/xtwitter.c
@@ -29,7 +29,7 @@
 
 #include "libtwitter.h"
 
-#define XTWITTER_WINDOW_WIDTH 500
+#define XTWITTER_WINDOW_WIDTH 480
 #define XTWITTER_WINDOW_HEIGHT 64
 
 Display *display;
@@ -76,7 +76,7 @@ int xtwitter_xinit()
                  &root_x, &root_y, &root_width, &root_height,
                  &root_border, &root_depth);
 
-    text_fonts = XCreateFontSet(display, "-*-*-medium-r-normal--16-*-*-*", 
+    text_fonts = XCreateFontSet(display, "-*-*-medium-r-normal--14-*-*-*", 
                                 &missing_list, &missing_count, &def_string);
     if(!text_fonts){
         printf( "error: XCreateFontSet\n" );
@@ -102,12 +102,42 @@ int xtwitter_xinit()
     return 0;
 }
 
+int utf8pos(const char *str, int width){
+    int i=0;
+    unsigned char c;
+    while(str[i] && width > 1){
+        c = (unsigned char)str[i];
+        width--;
+        if(c < 0x80){
+            i++;
+            continue;
+        }
+        width--;
+        while(c & 0x80){
+            c<<=1;
+            i++;
+        }
+    }
+    return i;
+}
+
 int xtwitter_show_status(twitter_t *twitter, twitter_status_t *status)
 {
     XSetWindowAttributes attr;
     char image_name[PATH_MAX];
     char image_path[PATH_MAX];
     Pixmap pixmap;
+    int pad_y;
+    int text_line=0;
+    int pos;
+    const char *text = status->text;
+    unsigned char c;
+    int i;
+    while(*text){
+        pos=utf8pos(text, 60);
+        text+=pos;
+        text_line++;
+    }
 
     twitter_image_name(status, image_name);
     snprintf(image_path, PATH_MAX, "%s/%s", twitter->images_dir, image_name);
@@ -124,18 +154,40 @@ int xtwitter_show_status(twitter_t *twitter, twitter_status_t *status)
     XChangeWindowAttributes(display, window, CWOverrideRedirect, &attr);
     XMapWindow(display, window);
 
-    XmbDrawString(display, window, text_fonts, gc, 55, 38,
-                  status->text, strlen(status->text));
+    switch(text_line){
+    case 1:
+        pad_y = 37;
+        break;
+    case 2:
+        pad_y = 26;
+        break;
+    case 3:
+        pad_y = 17;
+        break;
+    default:
+        pad_y = 15;
+    }
+    text = status->text;
+    i=0;
+    while(*text){
+        pos=utf8pos(text, 60);
+        XmbDrawString(display, window, text_fonts, gc,
+                      55, pad_y + 16 * i++, text, pos);
+        text+=pos;
+    }
     XmbDrawString(display, window, user_fonts, gc, 5, 60,
                   status->user->screen_name,
                   strlen(status->user->screen_name));
 
     imlib_image = Imlib_load_image(imlib_data, image_path);
-    Imlib_render(imlib_data, imlib_image, imlib_image->rgb_width, imlib_image->rgb_height);
-    pixmap = Imlib_move_image(imlib_data, imlib_image);
-    XCopyArea(display, pixmap, window, gc, 0, 0,
-              imlib_image->rgb_width, imlib_image->rgb_height,
-              5, 5); 
+    if(imlib_image){
+        Imlib_render(imlib_data, imlib_image,
+                     imlib_image->rgb_width, imlib_image->rgb_height);
+        pixmap = Imlib_move_image(imlib_data, imlib_image);
+        XCopyArea(display, pixmap, window, gc, 0, 0,
+                  imlib_image->rgb_width, imlib_image->rgb_height,
+                  5, 5); 
+    }
 
     XFlush(display);
     sleep(twitter->show_interval);
